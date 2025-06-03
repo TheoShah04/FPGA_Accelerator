abc263d1c953a9b622543177fd6debef0311556e
leo-sawatyanon <leeung.sawatyanon@gmail.com>
2025-06-03 17:47:10 +0100

    Fix inv_sqrt module Still need less than 0.5 shift

--- maths-accelerator/rayMarcher/VerilogVersion/invsqrt_bram.sv
+++ maths-accelerator/rayMarcher/VerilogVersion/invsqrt_bram.sv
@@ -1,12 +1,13 @@
 module invsqrt_bram #(
-    parameter ADDR_LENGTH = 12;
-              DATA_WIDTH = 32;
+    parameter ADDR_LENGTH = 12,
+              DATA_WIDTH = 32
 )(
-
+    input logic                   clk,
     input logic [ADDR_LENGTH-1:0] in_addr,
     input logic                   en,
-    output logic [DATA_WIDTH-1:0] out_sqrtinv;
-)
+    output logic [DATA_WIDTH-1:0] out_sqrtinv,
+    output logic                  valid_out
+);
 
     logic [DATA_WIDTH-1:0] data_out;
     
@@ -15,13 +16,14 @@ module invsqrt_bram #(
     logic [DATA_WIDTH-1:0] lut_rom [0:(1<<ADDR_LENGTH)-1];
 
     initial begin
-     $readmemh("lut_init.mem", lut_rom);
+     $readmemh("VerilogVersion/lut_init.mem", lut_rom);
     end
 
     // then in your logic: creates a an additional one clock memory to read from the BRAM
     always_ff @(posedge clk) begin
         if (en)
-            data_out <= lut_mem[in_addr];
+            data_out <= lut_rom[in_addr];
+            valid_out <= en;
     end
 
     assign out_sqrtinv = data_out;
