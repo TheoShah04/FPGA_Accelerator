b9dd52b48ed725f4cf9200431d4659a85e75422c
Theo Shah <theodoreshah1@gmail.com>
2025-06-04 17:26:01 +0100

    Finished vec3

--- maths-accelerator/rayMarcher/VivadoSim/VivadoSim.sim/sim_1/behav/xsim/xsim.dir/tb_inv_sqrt_behav/obj/xsim_1.c
+++ maths-accelerator/rayMarcher/VivadoSim/VivadoSim.sim/sim_1/behav/xsim/xsim.dir/tb_inv_sqrt_behav/obj/xsim_1.c
@@ -54,55 +54,52 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_16(char*, char *);
+IKI_DLLESPEC extern void execute_11(char*, char *);
+IKI_DLLESPEC extern void execute_12(char*, char *);
+IKI_DLLESPEC extern void execute_13(char*, char *);
+IKI_DLLESPEC extern void execute_14(char*, char *);
+IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_24(char*, char *);
+IKI_DLLESPEC extern void execute_25(char*, char *);
+IKI_DLLESPEC extern void execute_26(char*, char *);
+IKI_DLLESPEC extern void execute_27(char*, char *);
+IKI_DLLESPEC extern void execute_5(char*, char *);
+IKI_DLLESPEC extern void execute_6(char*, char *);
+IKI_DLLESPEC extern void execute_10(char*, char *);
+IKI_DLLESPEC extern void execute_22(char*, char *);
+IKI_DLLESPEC extern void execute_23(char*, char *);
+IKI_DLLESPEC extern void execute_8(char*, char *);
+IKI_DLLESPEC extern void execute_9(char*, char *);
+IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_for_reg(char*, char*, char*);
 IKI_DLLESPEC extern void execute_17(char*, char *);
 IKI_DLLESPEC extern void execute_18(char*, char *);
 IKI_DLLESPEC extern void execute_19(char*, char *);
 IKI_DLLESPEC extern void execute_20(char*, char *);
+IKI_DLLESPEC extern void execute_28(char*, char *);
+IKI_DLLESPEC extern void execute_29(char*, char *);
+IKI_DLLESPEC extern void execute_30(char*, char *);
 IKI_DLLESPEC extern void execute_31(char*, char *);
 IKI_DLLESPEC extern void execute_32(char*, char *);
 IKI_DLLESPEC extern void execute_33(char*, char *);
-IKI_DLLESPEC extern void execute_7(char*, char *);
-IKI_DLLESPEC extern void execute_15(char*, char *);
-IKI_DLLESPEC extern void execute_29(char*, char *);
-IKI_DLLESPEC extern void execute_30(char*, char *);
-IKI_DLLESPEC extern void execute_9(char*, char *);
-IKI_DLLESPEC extern void execute_10(char*, char *);
-IKI_DLLESPEC extern void execute_14(char*, char *);
-IKI_DLLESPEC extern void execute_27(char*, char *);
-IKI_DLLESPEC extern void execute_28(char*, char *);
-IKI_DLLESPEC extern void execute_12(char*, char *);
-IKI_DLLESPEC extern void execute_13(char*, char *);
-IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_for_reg(char*, char*, char*);
-IKI_DLLESPEC extern void execute_22(char*, char *);
-IKI_DLLESPEC extern void execute_23(char*, char *);
-IKI_DLLESPEC extern void execute_24(char*, char *);
-IKI_DLLESPEC extern void execute_25(char*, char *);
-IKI_DLLESPEC extern void execute_34(char*, char *);
-IKI_DLLESPEC extern void execute_35(char*, char *);
-IKI_DLLESPEC extern void execute_36(char*, char *);
-IKI_DLLESPEC extern void execute_37(char*, char *);
-IKI_DLLESPEC extern void execute_38(char*, char *);
-IKI_DLLESPEC extern void execute_39(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[31] = {(funcp)execute_16, (funcp)execute_17, (funcp)execute_18, (funcp)execute_19, (funcp)execute_20, (funcp)execute_31, (funcp)execute_32, (funcp)execute_33, (funcp)execute_7, (funcp)execute_15, (funcp)execute_29, (funcp)execute_30, (funcp)execute_9, (funcp)execute_10, (funcp)execute_14, (funcp)execute_27, (funcp)execute_28, (funcp)execute_12, (funcp)execute_13, (funcp)vlog_simple_process_execute_0_fast_for_reg, (funcp)execute_22, (funcp)execute_23, (funcp)execute_24, (funcp)execute_25, (funcp)execute_34, (funcp)execute_35, (funcp)execute_36, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 31;
+funcp funcTab[28] = {(funcp)execute_11, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_24, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_5, (funcp)execute_6, (funcp)execute_10, (funcp)execute_22, (funcp)execute_23, (funcp)execute_8, (funcp)execute_9, (funcp)vlog_simple_process_execute_0_fast_for_reg, (funcp)execute_17, (funcp)execute_18, (funcp)execute_19, (funcp)execute_20, (funcp)execute_28, (funcp)execute_29, (funcp)execute_30, (funcp)execute_31, (funcp)execute_32, (funcp)execute_33, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 28;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/tb_vec3Length_behav/xsim.reloc",  (void **)funcTab, 31);
+	iki_relocate(dp, "xsim.dir/tb_inv_sqrt_behav/xsim.reloc",  (void **)funcTab, 28);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
 
 void sensitize(char *dp)
 {
-	iki_sensitize(dp, "xsim.dir/tb_vec3Length_behav/xsim.reloc");
+	iki_sensitize(dp, "xsim.dir/tb_inv_sqrt_behav/xsim.reloc");
 }
 
 void simulate(char *dp)
 {
-		iki_schedule_processes_at_time_zero(dp, "xsim.dir/tb_vec3Length_behav/xsim.reloc");
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/tb_inv_sqrt_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
 
@@ -125,9 +122,9 @@ int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
     iki_set_xsimdir_location_if_remapped(argc, argv)  ;
-    iki_set_sv_type_file_path_name("xsim.dir/tb_vec3Length_behav/xsim.svtype");
-    iki_set_crvs_dump_file_path_name("xsim.dir/tb_vec3Length_behav/xsim.crvsdump");
-    void* design_handle = iki_create_design("xsim.dir/tb_vec3Length_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+    iki_set_sv_type_file_path_name("xsim.dir/tb_inv_sqrt_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/tb_inv_sqrt_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/tb_inv_sqrt_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
     return iki_simulate_design();
