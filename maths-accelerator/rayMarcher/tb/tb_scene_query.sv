7ecff181a893b6f0857ec1381852ed3a7f510480
Theo Shah <theodoreshah1@gmail.com>
2025-06-05 21:45:33 +0100

    Scene query stuff, sdfCube doesnt work but sdfSphere does

--- maths-accelerator/rayMarcher/tb/tb_scene_query.sv
+++ maths-accelerator/rayMarcher/tb/tb_scene_query.sv
@@ -4,6 +4,7 @@
 
 module tb_scene_query;
     logic clk;
+    logic rst_n = 1;
     logic valid_in;
     logic obj_sel;
     vec3 pos;
@@ -25,7 +26,7 @@ module tb_scene_query;
 
     sceneQuery dut (
         .clk(clk),
-        .rst(rst),
+        .rst(rst_n),
         .valid_in(valid_in),
         .pos(pos),
         .obj_sel(obj_sel), // just select cube for now
@@ -35,12 +36,17 @@ module tb_scene_query;
 
     initial begin
         $display("Testing sdfCube & scenequery");
-        obj_sel = 1'b1;
+
+        rst_n <= 0;
+        #20;
+        rst_n <= 1;
+
+        obj_sel = 1'b0;
         valid_in = 0;
         pos = '0;
         repeat(2) @(posedge clk);
         // at origin (should be negative)
-        pos = make_vec3(32'h00000000, 32'h00000000, 32'h00000000);
+        pos = make_vec3(32'h00000000, 32'h00000000, 32'h00000000); //origin, expect -0.1
         valid_in = 1;
         @(posedge clk);
         valid_in = 0;
@@ -52,7 +58,7 @@ module tb_scene_query;
         // $display("On Origin: " closestDistance[31] ? "neg" : "pos");
 
         // on edge (non-negative)
-        pos= make_vec3(32'h00333333, 32'h00000000, 32'h00000000);
+        pos= make_vec3(32'h00333333, 32'h00000000, 32'h00000000); //0.2, expect 0.1
         valid_in = 1;
         @(posedge clk);
         valid_in = 0;
@@ -63,18 +69,18 @@ module tb_scene_query;
         // $display("On edge: " closestDistance[31] ? "neg" : "pos");
 
         // // on edge (non-negative)
-        // pos= make_vec3(32'h0099999a, 32'h00000000, 32'h00000000);
-        // valid_in = 1;
-        // @(posedge clk);
-        // valid_in = 0;
-        // wait(valid_out == 1);
-        // @(posedge clk);
-        // test_count++;
-        // if (closestDistance[31] == 1'b0) pass_count++;
-        // // $display("On edge: " closestDistance[31] ? "neg" : "pos");
+        pos= make_vec3(32'h0099999a, 32'h00000000, 32'h00000000); //0.6, expect 0.5
+        valid_in = 1;
+        @(posedge clk);
+        valid_in = 0;
+        wait(valid_out == 1);
+        @(posedge clk);
+        test_count++;
+        if (closestDistance[31] == 1'b0) pass_count++;
+        // $display("On edge: " closestDistance[31] ? "neg" : "pos");
 
         // outside (always positive)
-        pos = make_vec3(32'h004ccccd, 32'h00000000, 32'h00000000);
+        pos = make_vec3(32'h004ccccd, 32'h00000000, 32'h00000000); //0.3, expect 0.2
         valid_in = 1;
         @(posedge clk);
         valid_in = 0;
@@ -83,6 +89,29 @@ module tb_scene_query;
         test_count++;
         if (closestDistance[31] == 1'b0) pass_count++;
         // $display("Outside: " closestDistance[31] ? "neg" : "pos");
+
+        // outside (always positive)
+        pos = make_vec3(32'h004ccccd, 32'h004ccccd, 32'h00000000); //0.3, 0.3,  expect 0.28284 for cube, and 0.32426 for sphere
+        valid_in = 1;
+        @(posedge clk);
+        valid_in = 0;
+        wait(valid_out == 1);
+        @(posedge clk);
+        test_count++;
+        if (closestDistance[31] == 1'b0) pass_count++;
+        // $display("Outside: " closestDistance[31] ? "neg" : "pos");
+
+         // outside (always positive)
+        pos = make_vec3(32'h00333333, 32'h00333333, 32'h00333333); //0.2, 0.2, 0.2,  expect 0.1732 for cube, and 0.24641 for sphere
+        valid_in = 1;
+        @(posedge clk);
+        valid_in = 0;
+        wait(valid_out == 1);
+        @(posedge clk);
+        test_count++;
+        if (closestDistance[31] == 1'b0) pass_count++;
+        // $display("Outside: " closestDistance[31] ? "neg" : "pos");
+
 
         $display("Passed: %0d/%0d", pass_count, test_count);
         
